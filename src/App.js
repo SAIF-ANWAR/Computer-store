6794a92a82b852ec53e68bfb9c7d2aeb9bc0f91e
SAIF-ANWAR <saifanwar5123@gmail.com>
2022-04-29 15:03:41 +0600

    route-setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import Login from './Pages/Shared/Login/Login';
+import SignUp from './Pages/Shared/SignUp/SignUp';
+import NotFound from './Pages/NotFound/NotFound';
+import Header from './Pages/Shared/Header/Header';
+
 
 function App() {
   return (
     <div>
-      <h2>Hello World!</h2>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/signup' element={<SignUp></SignUp>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+      </Routes>
     </div>
   );
 }
