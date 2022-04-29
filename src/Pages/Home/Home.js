db1d670fbd5b5914603c6ddfd971881f98cb3cee
SAIF-ANWAR <saifanwar5123@gmail.com>
2022-04-30 00:55:59 +0600

    carousel-done

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,46 +3,38 @@ import { Carousel } from 'react-bootstrap';
 import banner1 from '../../images/banner/banner1.jpg';
 import banner2 from '../../images/banner/banner2.jpg';
 import banner3 from '../../images/banner/banner3.jpg';
-// import banner5 from '../../images/banner/banner5.jpg';
 import './Home.css'
 
 const Home = () => {
     return (
-        <Carousel fade>
-            <Carousel.Item interval={1000}>
-                <img
-                    className="d-block w-100"
-                    src={banner1}
-                    alt="First slide"
-                />
-                <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item interval={500}>
-                <img
-                    className="d-block w-100"
-                    src={banner2}
-                    alt="Second slide"
-                />
-                <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src={banner3}
-                    alt="Third slide"
-                />
-                <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-        </Carousel>
+        <div >
+            <Carousel fade>
+                <Carousel.Item interval={1000}>
+                    <img
+                        className="d-block w-100"
+                        src={banner1}
+                        alt="First slide"
+                    />
+
+                </Carousel.Item>
+                <Carousel.Item interval={800}>
+                    <img
+                        className="d-block w-100"
+                        src={banner2}
+                        alt="Second slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={banner3}
+                        alt="Third slide"
+                    />
+                </Carousel.Item>
+            </Carousel>
+
+
+        </div>
     );
 };
 
