fda4efc029cc2206fd2aec725d498c88c769c1a5
SAIF-ANWAR <saifanwar5123@gmail.com>
2022-04-30 03:08:08 +0600

    login-page-done

--- src/Pages/Shared/Login/Login.js
+++ src/Pages/Shared/Login/Login.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import login from '../../../images/login.jpg';
+import SocialLogin from '../SocialLogin/SocialLogin';
+import './Login.css'
 
 const Login = () => {
     return (
-        <div>
-            <h1>log in</h1>
+        <div className='container py-5'>
+            <div className='row'>
+                <div className='col-lg-6 col-sm-12  '>
+                    <img className='mx-auto' src={login} alt="" />
+                </div>
+                <div className='col-lg-6 col-sm-12'>
+                    <div className='w-75 mx-auto '>
+                        <Form className=' mt-3 '>
+                            <h3 className='mb-3'>Please Log In</h3>
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+
+                                <Form.Control type="email" placeholder="Enter email" />
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="formBasicPassword">
+
+                                <Form.Control type="password" placeholder="Password" />
+                            </Form.Group>
+                            <div className='d-flex justify-content-between px-1'>
+                                <p>Don't have an account?</p>
+                                <Link to="/signup">Create account</Link>
+                            </div>
+
+                            <Button variant="outline-primary" type="submit">
+                                Login
+                            </Button>
+                        </Form>
+                    </div>
+                    <SocialLogin></SocialLogin>
+                </div>
+            </div>
         </div>
     );
 };
---
+++ src/Pages/Shared/SocialLogin/SocialLogin.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import google from '../../../images/google.png'
+
+const SocialLogin = () => {
+    return (
+        <div>
+            <div className='w-75 mt-4 mx-auto'>
+                <div className='d-flex align-items-center'>
+                    <div style={{ height: '1px' }} className="bg-primary w-50 " ></div>
+                    <div className='px-3'>Or</div>
+                    <div style={{ height: '1px' }} className="bg-primary w-50" ></div>
+                </div>
+                <div className='mt-4'>
+                    <Button variant="outline-primary" className='d-block mx-auto mt-2 '>
+                        <img className='px-1' width={"30px"} src={google} alt="" />
+                        Sign in with Google</Button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SocialLogin;
